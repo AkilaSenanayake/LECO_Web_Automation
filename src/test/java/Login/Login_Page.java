f3834f42c81b1cab5ffa5df8b9be92fd60fff5da
Akila Senanayake <akilasenanayake22@gmail.com>
2026-01-12 10:25:34 +0530

    Enhance POM file structure and improve login page tests with explicit waits; remove unused PasswordInfo class

--- src/test/java/Login/Login_Page.java
+++ src/test/java/Login/Login_Page.java
@@ -1,18 +1,25 @@
 package Login;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
 
 @Listeners(testNG.Listeners.LoginListeners.class)
 public class Login_Page {
 
     WebDriver driver;
+    SoftAssert softAssert = new SoftAssert();
 
     @BeforeMethod
     public void Beforemethod(){
@@ -25,20 +32,33 @@ public class Login_Page {
     //Test login with empty fields
     @Test
     public void LECO_LP_WEB_001(){
-        WebElement loginBtn = driver.findElement();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        // Click Login button without entering data
+        WebElement loginBtn = wait.until(ExpectedConditions.elementToBeClickable(By.id("loginBtn")));
         loginBtn.click();
 
-        //Have to add waits
-        WebElement validationMsg = driver.findElement();
-        String actualMsg = validationMsg.getText();
+        // Wait until validation email message is visible
+        WebElement validationMsg1 = wait.until(ExpectedConditions.
+                visibilityOfElementLocated(By.xpath(""))); //Email field element
 
-        System.out.println("Validation message: "+actualMsg);
+        String actualMsg1 = validationMsg1.getText();
+        String expectedValue1 = "E-mail is required";
+        softAssert.assertEquals(actualMsg1,expectedValue1,"Email validation miss matched");
 
-        driver.quit();
+        // Wait until validation password message is visible
+        WebElement validationMsg2 = wait.until(ExpectedConditions.
+                visibilityOfElementLocated(By.xpath(""))); //Email field element
+
+        String actualMsg2 = validationMsg2.getText();
+        String expectedValue2 = "E-mail is required";
+        softAssert.assertEquals(actualMsg1,expectedValue1,"Email validation miss matched");
+
+        System.out.println("Validation message: " + actualMsg1);
     }
 
     //Login with invalid inputs
-    @Test(dataProvider = "LoginInfo",dataProviderClass = LoginInfo.class)
+    @Test
     public void LECO_LP_WEB_002__003__004(String email, String password){
         WebElement eMail = driver.findElement();
         eMail.sendKeys(email);
